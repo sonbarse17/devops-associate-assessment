22ace3fa4a68e0f03b133d7d4cd19b6fc70fa541
Sushant Sonbarse <sushantsonbarse90@gmail.com>
2025-06-06 11:11:52 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+# ec2.tf
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Environment = "DevOpsTest"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
